9b81c4b9ca288e552ab5ae29f786d15fb7be4bf0
pratik barde <pratik.barde@calfus.com>
2024-03-14 12:22:29 +0530

    DSA Practice

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import dsa.binarysearchtree.BinarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Binary Search Tree");
+        BinarySearchTree bst = new BinarySearchTree();
+        System.out.println("BST root = "+ bst.root);
+
+
+        System.out.println(bst.insert(24));
+        System.out.println(bst.insert(24));
+        System.out.println(bst.insert(30));
+        System.out.println(bst.insert(35));
+        System.out.println(bst.insert(32));
+
+//        System.out.println("value = "+ bst.root.left.right.value);
+        bst.contains(119);
+
+        System.out.println(bst.rContains(24));
+
+    }
+}
---
+++ src/dsa/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,95 @@
+package dsa.binarysearchtree;
+
+public class BinarySearchTree {
+
+    public Node root = new Node(27);
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+
+
+    public boolean insert(int value){
+
+        Node newNode = new Node(value);
+        if (root==null){
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            if(temp.value == newNode.value){
+                return false;
+            }
+            if(temp.value > newNode.value){
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }else{
+                    temp = temp.left;
+                }
+            }
+
+            if(temp.value < newNode.value){
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }else{
+                    temp = temp.right;
+                }
+            }
+
+        }
+
+    }
+
+    public boolean contains(int value){
+
+        Node temp = root;
+        while(temp!=null){
+            if(temp.value == value){
+                System.out.println("ELEMENT FOUND!");
+                return true;
+            }
+            if(temp.value > value){
+                temp = temp.left;
+            }else{
+                temp = temp.right;
+            }
+
+        }
+        System.out.println("ELEMENT NOT FOUND");
+        return false;
+    }
+
+
+    private boolean rContains(Node current_node, int value){
+
+        if(current_node == null){
+            return false;
+        }
+        if(current_node.value == value){
+            return true;
+        }
+        if(current_node.value > value){
+            return rContains(current_node.left, value);
+        }else{
+            return rContains(current_node.right, value);
+        }
+
+    }
+
+    public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+
+}
